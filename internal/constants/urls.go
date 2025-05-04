ef454225e5a7908262c9bc6855dc7deacd6d1d42
heywinit <heywinit@gmail.com>
2025-05-04 19:38:07 +0530

    added a service for contacting la api du meteora

--- internal/constants/urls.go
+++ internal/constants/urls.go
@@ -1,5 +1,5 @@
 package constants
 
 const (
-    BaseURL         = "https://api.example.com" // Base URL for the API
+	MeteoraPubBaseURL = "https://keeper.infra.trading/api/v1" // Base URL for the Meteora API
 )
